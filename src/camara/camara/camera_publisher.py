2432687b4ca1e1ddda05c4816c7601436d32e956
darhf <didier.hernandez1972@gmail.com>
2025-05-02 17:15:11 -0600

    Cambio a package y a publisher

--- src/camara/camara/camera_publisher.py
+++ src/camara/camara/camera_publisher.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import threading
+import threading, sys
 
 import rclpy
 from rclpy.node import Node
@@ -11,31 +11,24 @@ class CameraPublisher(Node):
     def __init__(self):
         super().__init__('camera_publisher_node')
 
-        # Construye el pipeline GStreamer (60 fps, resolución 640×480)
         gst_pipeline = (
             "nvarguscamerasrc ! "
             "video/x-raw(memory:NVMM),width=640,height=480,format=NV12,framerate=60/1 ! "
-            "videorate ! video/x-raw,framerate=60/1 ! "
             "nvvidconv ! video/x-raw,format=BGRx ! "
+            "videorate ! video/x-raw,format=BGRx,framerate=60/1 ! "
             "videoconvert ! video/x-raw,format=BGR ! appsink"
         )
 
-        # Abre la cámara CSI
         self.cap = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)
         if not self.cap.isOpened():
             self.get_logger().error("❌ No se pudo abrir la cámara CSI con GStreamer.")
-            rclpy.shutdown()
-            return
+            raise SystemExit(1)
 
-        # Publisher de Image
         self.publisher = self.create_publisher(Image, '/camera/image_raw', 10)
         self.get_logger().info("📷 Nodo de cámara iniciado (60 Hz)")
 
-        # Bandera para parar el hilo y Rate a 60 Hz
         self._stop = False
         self._rate = self.create_rate(60)
-
-        # Lanzar hilo de captura continua
         threading.Thread(target=self._spin_read, daemon=True).start()
 
     def _spin_read(self):
@@ -52,16 +45,13 @@ class CameraPublisher(Node):
                 msg.header.stamp = self.get_clock().now().to_msg()
                 msg.header.frame_id = "camera_frame"
                 self.publisher.publish(msg)
-            # Esperar al siguiente ciclo (60 Hz)
             self._rate.sleep()
 
     def destroy_node(self):
-        # Señalar al hilo que pare y liberar recursos
         self._stop = True
-        if hasattr(self, 'cap') and self.cap.isOpened():
+        if self.cap.isOpened():
             self.cap.release()
         super().destroy_node()
-
 
 def main(args=None):
     rclpy.init(args=args)
@@ -74,7 +64,6 @@ def main(args=None):
         node.destroy_node()
         cv2.destroyAllWindows()
         rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
